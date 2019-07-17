ab094cc0c3689466f6f21f40fbedaf98e2f77327
Lucas Travagin <lucas_travagin1112@hotmail.com>
2019-07-17 03:40:13 -0300

    Criando o Dashboard Controller

---
+++ frontend/app/config/consts.js
@@ -0,0 +1,9 @@
+app.constant('consts', {
+    appName: 'Poke APP',
+    version: '1.0',
+    owner: 'Lucas Travagin',
+    year: '2019',
+    apiUrl: 'http://localhost:3003/api'
+}).run(['$rootScope', 'consts', function($rootScope, consts) {
+    $rootScope.consts = consts
+}])
---
+++ frontend/app/dashboard/dasboard.controller.js
@@ -0,0 +1,17 @@
+app.controller('DashboardCtrl', [
+    '$http',
+    'consts',
+    DashboardController
+])
+
+function DashboardController($http, consts) {
+    const vm = this
+
+    vm.datas = { pokemons: 2.852 }
+
+    vm.getSummary = function() {
+        vm.total = vm.datas
+    }
+
+    vm.getSummary()
+}
