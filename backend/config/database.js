064517bea0fb2215dd749af38d9cd02e81f24a48
Lucas Travagin <lucas_travagin1112@hotmail.com>
2019-07-17 21:02:55 -0300

    Dando inicio ao Controller Pokemon, criando os métodos de requisições post e get

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
-module.exports = mongoose.createConnection('mongodb://localhost/pokeapp')
+
+mongoose.Promise = global.Promise;
+module.exports = mongoose.connect('mongodb://localhost/pokeapp', { useNewUrlParser: true, keepAlive: true, reconnectTries: Number.MAX_VALUE, useMongoClient: true })
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
--- frontend/app/pokemon/pokemon.controller.js
+++ frontend/app/pokemon/pokemon.controller.js
@@ -1,29 +1,65 @@
-app.controller('PokemonCtrl', [
-    'tabs',
-    PokemonController
+(function() {
+
+    app.controller('PokemonCtrl', [
+        'tabs',
+        '$http',
+        'consts',
+        PokemonController
 
     ])
 
-    function PokemonController(tabs) {
+    function PokemonController(tabs, $http, consts) {
 
         const vm = this
+        const url = `${consts.apiUrl}/pokemons`
+
+        let initAtackAndDefense = () => {
+            if (!vm.pokemon.atack || !vm.pokemon.atack.length) {
+                vm.pokemon.atack = []
+                vm.pokemon.atack.push({})
+            }
+
+            if (!vm.pokemon.defense || !vm.pokemon.defense.length) {
+                vm.pokemon.defense = []
+                vm.pokemon.defense.push({})
+            }
+        }
 
         //Busca os pokemons
         vm.getPokemons = () => {
-            tabs.show(vm, {tabList: true, tabCreate: true})
+            $http.get(url).then((resp) => {
+                vm.pokemons = resp.data
+                vm.pokemon = {}
+                initAtackAndDefense()
+                tabs.show(vm, { tabList: true, tabCreate: true })
+            })
         }
-        
-        vm.getPokemons()
+
+        vm.createPokemon = () => {
+            $http.post(url, vm.pokemon).then((resp) => {
+                vm.billingCycle = {}
+            }).catch((err) => {
+
+            })
+        }
+
+
+
+
 
         vm.showTabUpdate = () => {
-            tabs.show(vm, {tabUpdate: true})
+            tabs.show(vm, { tabUpdate: true })
         }
 
-         vm.showTabDelete = () => {
-            tabs.show(vm, {tabDelete: true})
+        vm.showTabDelete = () => {
+            tabs.show(vm, { tabDelete: true })
         }
 
         vm.cancel = () => {
-            tabs.show(vm, {tabList: true, tabCreate: true})
+            tabs.show(vm, { tabList: true, tabCreate: true })
         }
+
+        vm.getPokemons()
     }
+
+})()
